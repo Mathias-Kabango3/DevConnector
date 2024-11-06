d8c42da446d8bade388adabeb02a547c179226c0
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-11-06 13:23:59 +0200

    connect to mongodb

--- server.js
+++ server.js
@@ -1,12 +1,17 @@
 const express = require('express');
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on port ${port}`);
 });

5e9602bc472e94c8935975e74f9500482e95cce4
Mathias-Kabango3 <m.kabango@alustudent.com>
2024-11-06 14:02:23 +0200

    routes added

--- server.js
+++ server.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
+const Users = require("./routes/users");
+const Profile = require("./routes/profile");
+const Posts = require("./routes/posts");
 
 dotenv.config();
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+// use routes
+app.use("/api/users", Users);
+app.use("/api/profile", Profile);
+app.use("/api/posts", Posts);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
